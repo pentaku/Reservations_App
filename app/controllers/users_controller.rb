2e15584bb67bde89fde427b5337e6f1379449880
pentaku <ohara1728136@gmail.com>
2025-08-06 22:52:43 +0900

    user登録メッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,27 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  # Post /users(+ params) => users#create
+  # name,email,password,password_confirmation以外は受け付けない
+  # マスアサイメント脆弱性で[:admin]とか送られると権限が書き換えられてしまう可能性。
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome !"
+      redirect_to @user #Get "/users/#{@user.id}"
+    else
+      render 'new'
+    end
+  end
+
+
+  # =============================================================
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
+  end
+
 end
