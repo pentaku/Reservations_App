f56471664e75cf3cc17d9ca3d6fce859919e6e59
pentaku <ohara1728136@gmail.com>
2025-08-10 12:51:07 +0900

    js/packs/applivation.js:require('jquery')の追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 
+// 追記
+require('jquery')
+
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
