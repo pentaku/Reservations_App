1888a662800e27695af6a58908476f9b08934350
pentaku <ohara1728136@gmail.com>
2025-08-12 20:51:41 +0900

    correct_userの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -39,6 +43,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+  end
+
 
   # =============================================================
   private
@@ -62,7 +69,7 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     # ログインしていないと、current_userが呼び出せない。
-    redirect_to(root_url) unless @user == current_user?(@user)
+    redirect_to(root_url) unless current_user?(@user)
   end
 
 end
