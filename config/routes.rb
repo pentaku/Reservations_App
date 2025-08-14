289f76180b0981b49bb9848586a9389be153730b
pentaku <ohara1728136@gmail.com>
2025-08-15 07:32:04 +0900

    accountとprofileページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   # usersコントローラ
+  get 'users/account', to: 'users#account'
+  get 'users/profile', to: 'users#profile'
   get 'users/:id/edit_password', to: 'users#edit_password'
   patch '/update_password', to: 'users#update_password'
   resources :users
