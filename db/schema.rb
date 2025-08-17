cbf67b4b3e239bf6d518c50420fb46c2109f709a
pentaku <ohara1728136@gmail.com>
2025-08-17 21:51:15 +0900

    rails g model Reservation user:references room:references check_in:date check_out:date guests:integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_08_16_020936) do
+ActiveRecord::Schema.define(version: 2025_08_17_123920) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.date "check_in"
+    t.date "check_out"
+    t.integer "guests"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_reservations_on_room_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -33,5 +45,7 @@ ActiveRecord::Schema.define(version: 2025_08_16_020936) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
   add_foreign_key "rooms", "users"
 end
